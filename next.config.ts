691deab6097644faa1d1a37296d539a76d4a5241
Soviet117 <adixontisg31@gmail.com>
2026-06-10 10:15:21 -0500

    Feax: UX/UI del menu

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   //output: 'standalone',
-  reactStricMode: true,
+  reactStrictMode: true,
   swcMinify: true,
   // Reinicio forzado para Prisma
+  // Touch to trigger reload
 };
 
 export default nextConfig;
